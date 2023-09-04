1ce6babebd57249b78acce51ce9175aafcc01da2
syedshaon <syedshaon99@gmail.com>
2023-09-04 19:03:04 +0600

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.js",
-  },
 });
